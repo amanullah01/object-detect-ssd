3133cfb265fb23635ddde81ca5295c52a564aad5
Amanullah Aman <amancse.aiub@gmail.com>
2024-05-17 06:23:29 +0600

    Construct class names in list

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import cv2
 import numpy as np
 
+
+def construct_class_names(file_name="class_names"):
+    with open(file_name, 'rt') as file:
+        names = file.read().rstrip('\n').split('\n')
+    return names
+
+
+class_names = construct_class_names()
+print(class_names)
